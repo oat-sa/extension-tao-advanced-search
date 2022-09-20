ee4a1cbdb822bc6eae53501ca792600c3b9102fc
Ivan Klimchuk <ivan@taotesting.com>
2022-09-20 14:58:34 +0300

    Add check of service existence before retrieving its options

--- migrations/Version202209081525261488_taoAdvancedSearch.php
+++ migrations/Version202209081525261488_taoAdvancedSearch.php
@@ -4,12 +4,16 @@ declare(strict_types=1);
 
 namespace oat\taoAdvancedSearch\migrations;
 
+use common_Exception;
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\Exception\IrreversibleMigration;
 use oat\generis\model\DependencyInjection\ServiceOptions;
 use oat\oatbox\event\EventManagerAwareTrait;
 use oat\oatbox\service\ConfigurableService;
+use oat\oatbox\service\exception\InvalidServiceManagerException;
+use oat\oatbox\service\ServiceNotFoundException;
 use oat\tao\model\search\index\IndexUpdaterInterface;
+use oat\tao\model\search\Search;
 use oat\tao\model\search\SearchProxy;
 use oat\tao\scripts\tools\migrations\AbstractMigration;
 use oat\taoAdvancedSearch\model\SearchEngine\Driver\Elasticsearch\ElasticSearch;
@@ -25,21 +29,29 @@ final class Version202209081525261488_taoAdvancedSearch extends AbstractMigratio
         return 'Fix legacy configuration';
     }
 
+    /**
+     * @throws ServiceNotFoundException
+     * @throws InvalidServiceManagerException
+     * @throws common_Exception
+     */
     public function up(Schema $schema): void
     {
         /** @var SearchProxy $searchProxy */
-        $searchProxy = $this->getServiceManager()->get(SearchProxy::SERVICE_ID);
+        $searchProxy = $this->getServiceManager()->get(Search::SERVICE_ID);
 
         /** @var ConfigurableService $oldElasticSearch */
         $oldElasticSearch = $searchProxy->getAdvancedSearch();
 
         /** @var ServiceOptions $serviceOptions */
         $serviceOptions = $this->getServiceManager()->get(ServiceOptions::SERVICE_ID);
-        $serviceOptions->save(ElasticSearchConfig::class, 'hosts', $oldElasticSearch->getOption('hosts'));
+
+        if ($oldElasticSearch) {
+            $serviceOptions->save(ElasticSearchConfig::class, 'hosts', $oldElasticSearch->getOption('hosts'));
+        }
 
         $searchProxy->setOption(SearchProxy::OPTION_ADVANCED_SEARCH_CLASS, ElasticSearch::class);
 
-        $this->registerService(SearchProxy::SERVICE_ID, $searchProxy);
+        $this->registerService(Search::SERVICE_ID, $searchProxy);
         $this->registerService(ServiceOptions::SERVICE_ID, $serviceOptions);
         $this->registerService(IndexUpdaterInterface::SERVICE_ID, new IndexUpdater());
     }
