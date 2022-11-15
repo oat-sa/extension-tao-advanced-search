ce8e1f697aaf6a1060e4e8fcccb0666a48784c81
Héctor Arroyo <hector.arroyo@taotesting.com>
2022-11-15 13:16:49 +0100

    feat: Apply document transoformations in ElasticSearchIndexer

--- model/SearchEngine/Driver/Elasticsearch/ElasticSearchIndexer.php
+++ model/SearchEngine/Driver/Elasticsearch/ElasticSearchIndexer.php
@@ -22,14 +22,20 @@ declare(strict_types=1);
 
 namespace oat\taoAdvancedSearch\model\SearchEngine\Driver\Elasticsearch;
 
+use oat\oatbox\service\ServiceManager;
+use oat\tao\model\search\index\DocumentBuilder\IndexDocumentBuilder;
 use oat\tao\model\search\index\IndexDocument;
 use Elasticsearch\Client;
+use oat\tao\model\search\SearchInterface;
+use oat\tao\model\search\SearchProxy;
+use oat\taoAdvancedSearch\model\Resource\Service\DocumentTransformation\TestTransformationStrategy;
 use oat\taoAdvancedSearch\model\SearchEngine\Contract\IndexerInterface;
 use oat\taoAdvancedSearch\model\SearchEngine\Service\IndexPrefixer;
 use Psr\Log\LoggerInterface;
 use Exception;
 use Iterator;
 use RuntimeException;
+use taoQtiTest_models_classes_QtiTestService;
 use Throwable;
 
 class ElasticSearchIndexer implements IndexerInterface
@@ -123,7 +129,11 @@ class ElasticSearchIndexer implements IndexerInterface
                 $this->logBatchFlush($this->logger, __METHOD__, $params);
 
                 $response = $this->client->bulk($params);
-                $this->logErrorsFromResponse($this->logger, $document, $response);
+                $this->logErrorsFromResponse(
+                    $this->logger,
+                    $document,
+                    $response
+                );
 
                 $count += $blockSize;
                 $blockSize = 0;
@@ -201,8 +211,42 @@ class ElasticSearchIndexer implements IndexerInterface
         return $document;
     }
 
-    private function extendBatch(string $action, string $indexName, IndexDocument $document, array $params): array
-    {
+    private function extendBatch(
+        string $action,
+        string $indexName,
+        IndexDocument $document,
+        array $params
+    ): array {
+        $this->logger->debug(
+            sprintf(
+                "%s::extendBatch: input = %s",
+                self::class,
+                var_export($document, true)
+            )
+        );
+
+        // Apply transformations
+        // @todo IoD
+        //
+        $transformations = [
+            new TestTransformationStrategy(
+                $this->logger,
+                $this->getDocumentBuilder(),
+                $this->getSearchService(),
+                $this->getQtiTestService()
+            )
+        ];
+
+        $resource = new \core_kernel_classes_Resource($document->getId());
+
+        foreach ($transformations as $strategy) {
+            $this->logger->critical(
+                sprintf('Applying transformation: %s', get_class($strategy))
+            );
+
+            $document = $strategy->transform($resource, $document);
+        }
+
         $params['body'][] = [
             $action => [
                 '_index' => $indexName,
@@ -224,8 +268,39 @@ class ElasticSearchIndexer implements IndexerInterface
             $body = ['doc' => $body];
         }
 
+        $this->logger->debug(
+            sprintf(
+                "%s::extendBatch: body = %s",
+                self::class,
+                var_export($body, true)
+            )
+        );
+
         $params['body'][] = $body;
 
         return $params;
+    }
+
+    private function getQtiTestService(): taoQtiTest_models_classes_QtiTestService
+    {
+        return $this->getService(taoQtiTest_models_classes_QtiTestService::class);
+    }
+
+    /**
+     * @fixme use DI
+     */
+    private function getService(string $serviceId)
+    {
+        return ServiceManager::getServiceManager()->get($serviceId);
+    }
+
+    private function getDocumentBuilder(): IndexDocumentBuilder
+    {
+        return $this->getService(IndexDocumentBuilder::class);
+    }
+
+    private function getSearchService(): SearchInterface
+    {
+        return $this->getService(SearchProxy::SERVICE_ID);
     }
 }
