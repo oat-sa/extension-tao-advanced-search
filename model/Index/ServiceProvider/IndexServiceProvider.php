d64fdabd045d0b960005c9a6e61ac80372b92f01
Bartłomiej Marszał <bartlomiejmarszal@gmail.com>
2025-03-26 11:00:11 +0100

    feat: update use block

--- model/Index/ServiceProvider/IndexServiceProvider.php
+++ model/Index/ServiceProvider/IndexServiceProvider.php
@@ -23,17 +23,12 @@ declare(strict_types=1);
 namespace oat\taoAdvancedSearch\model\Index\ServiceProvider;
 
 use oat\generis\model\DependencyInjection\ContainerServiceProviderInterface;
-use oat\tao\model\AdvancedSearch\AdvancedSearchChecker;
 use oat\tao\model\search\index\DocumentBuilder\IndexDocumentBuilderInterface;
 use oat\taoAdvancedSearch\model\Index\Service\AdvancedSearchIndexDocumentBuilder;
-use oat\taoAdvancedSearch\model\Index\Service\RecreatingIndexService;
-use oat\taoAdvancedSearch\model\Resource\Service\IndiciesConfigurationService;
 use oat\taoAdvancedSearch\model\Test\Normalizer\TestNormalizer;
 use oat\taoMediaManager\model\relation\service\IdDiscoverService;
 use oat\taoQtiItem\model\qti\parser\ElementReferencesExtractor;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
-use Elastic\Elasticsearch\Client;
-use oat\taoAdvancedSearch\model\SearchEngine\Service\IndexPrefixer;
 
 use function Symfony\Component\DependencyInjection\Loader\Configurator\service;
 
